fb8e8f1a7f4d6c2431848c605c24dd47ea358e80
lisijie <lsj86@qq.com>
2015-06-25 00:30:23 +0800

    完善功能

---
+++ template.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+var (
+	ThemeDir    = "themes"
+	TplVarLeft  = "{{"
+	TplVarRight = "}}"
+	T           *template.Template
+)
+
+func init() {
+	T = template.New("__top__")
+	funcMap := make(template.FuncMap)
+	funcMap["str2html"] = func(raw string) template.HTML {
+		return template.HTML(raw)
+	}
+
+	T.Funcs(funcMap)
+}
+
+func BuildTemplates(theme string) {
+	root := filepath.Join(workPath, ThemeDir, theme)
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		if filepath.Ext(path) == ".html" {
+			source, err := ioutil.ReadFile(path)
+			checkError(err)
+			_, err = T.New(info.Name()).Parse(string(source))
+			checkError(err)
+		}
+		return nil
+	})
+}
+
+func RenderTemplate(w io.Writer, tpl string, data interface{}) error {
+	return T.ExecuteTemplate(w, tpl, data)
+}
